86048eeb9b430d6549da843acffbb574bfb2a028
Yorsalem Feseha <yorsalemfeseha@Cyrus-MacBook-Air.local>
2020-05-22 18:26:23 +0100

    game update

---
+++ Game.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+  $('.header').height($(window).height());
+})
+
+
+// main js //
+const questionText=document.querySelector(".question-text");
+
+myQuiz=[
+          {
+            question:"Who plays the voice of Gossip Girl on the television show?",
+            options:["Kristen Bell", "Blake Lively", "Leighton Meester", "Taylor Momsen"],
+            answer:0,
+            description:
+          },
+          {
+            question:"Who plays the voice of Gossip Girl on the television show?",
+            options:["Kristen Bell", "Blake Lively", "Leighton Meester", "Taylor Momsen"],
+            answer:0,
+            description:
+          },
+          {
+            question:"Who plays the voice of Gossip Girl on the television show?",
+            options:["Kristen Bell", "Blake Lively", "Leighton Meester", "Taylor Momsen"],
+            answer:0,
+            description:
+          },
+          {
+            question:"Who plays the voice of Gossip Girl on the television show?",
+            options:["Kristen Bell", "Blake Lively", "Leighton Meester", "Taylor Momsen"],
+            answer:0,
+            description:
+          }, 
+
+
+
+        ]
